291d2ab1542879d7e69c71ec293bb3a9a34580fb
kiritokunti <duman200354@mail.ru>
2023-05-30 08:34:27 +0600

    feat: valudation rules for regiter form

--- src/components/TheAuth.vue
+++ src/components/TheAuth.vue
@@ -54,55 +54,62 @@
               </button>
             </form>
             <!-- Registration Form -->
-            <vee-form v-show="tab == 'register'">
+            <vee-form v-show="tab == 'register'" :validation-schema="schema" @submit="register">
               <!-- Name -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Name</label>
                 <vee-field name="name" type="text"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                   placeholder="Enter Name" />
+                <ErrorMessage class="text-red-600" name="name"/>
               </div>
               <!-- Email -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Email</label>
-                <input type="email"
+                <vee-field name="email" type="email"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                   placeholder="Enter Email" />
+                <ErrorMessage class="text-red-600" name="email"/>
               </div>
               <!-- Age -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Age</label>
-                <input type="number"
+                <vee-field name="age" type="number"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" />
+                <ErrorMessage class="text-red-600" name="age"/>
               </div>
               <!-- Password -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Password</label>
-                <input type="password"
+                <vee-field name="password" type="password"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                   placeholder="Password" />
+                <ErrorMessage class="text-red-600" name="password"/>
               </div>
               <!-- Confirm Password -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Confirm Password</label>
-                <input type="password"
+                <vee-field name="confirm_password" type="password"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                   placeholder="Confirm Password" />
+                <ErrorMessage class="text-red-600" name="confirm_password"/>
               </div>
               <!-- Country -->
               <div class="mb-3">
                 <label class="inline-block mb-2">Country</label>
-                <select
+                <vee-field as="select" name="country"
                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded">
                   <option value="USA">USA</option>
                   <option value="Mexico">Mexico</option>
                   <option value="Germany">Germany</option>
-                </select>
+                </vee-field>
+                <ErrorMessage class="text-red-600" name="country"/>
               </div>
               <!-- TOS -->
               <div class="mb-3 pl-6">
-                <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+                <vee-field name="tos" value="1" type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
                 <label class="inline-block">Accept terms of service</label>
+                <ErrorMessage class="text-red-600 block" name="tos"/>
               </div>
               <button type="submit"
                 class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700">
@@ -118,23 +125,37 @@
 <script>
     import { mapState, mapWritableState } from 'pinia';
     import useModalStore from '@/stores/modal'
+import { ErrorMessage } from 'vee-validate';
 
     export default {
-        data() {
-            return {
-                tab: 'login',
+    data() {
+        return {
+            tab: "login",
+            schema: {
+                name: "required|min:3|max:100|alphaSpaces",
+                email: "required|min:3|max:100|email",
+                age: "required|minValue:18|maxValue:100",
+                password: "required|min:8|max:16",
+                confirm_password: "confirmed:@password",
+                country: "required|excluded:Germany",
+                tos: "required",
             }
+        };
+    },
+    computed: {
+        ...mapState(useModalStore, ["hiddenClass"]),
+        ...mapWritableState(useModalStore, ["isOpen"])
+    },
+    methods: {
+        closeModal() {
+            this.isOpen = false;
         },
-        computed: {
-            ...mapState(useModalStore, ['hiddenClass']),
-            ...mapWritableState(useModalStore, ['isOpen'])
-        },
-        methods: {
-            closeModal() {
-                this.isOpen = false;
-            }
+        register(values) {
+          console.log(values);
         }
-    }
+    },
+    components: { ErrorMessage }
+}
 </script>
 
 <style lang="scss" scoped>
