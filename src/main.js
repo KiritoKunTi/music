5e9cd15415ac2d42234337f765489068aa86b497
kiritokunti <duman200354@mail.ru>
2023-06-16 18:25:20 +0600

    feat(main): init firebase first

--- src/main.js
+++ src/main.js
@@ -5,14 +5,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import VeeValidatePlugin from '@/includes/validation'
 import '@/includes/firebase'
-
+import { auth } from '@/includes/firebase'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+let app;
 
-app.use(createPinia())
-app.use(router)
-app.use(VeeValidatePlugin)
+auth.onAuthStateChanged(() => {
+    if(!app) {
+        app = createApp(App)
+        app.use(createPinia())
+        app.use(router)
+        app.use(VeeValidatePlugin)
 
-app.mount('#app')
+        app.mount('#app')
+    }
+})
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { setDoc, doc } from 'firebase/firestore'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
 import { auth, db } from '@/includes/firebase'
 
 export default defineStore('user', {
@@ -16,6 +16,9 @@ export default defineStore('user', {
                     this.userInfo = res.user
                 }
             )
+            await updateProfile(this.userInfo, {
+                displayName: values.name,
+            })
             await setDoc(doc(db, 'users', this.userInfo.uid), {
                 name: values.name,
                 email: values.email,
