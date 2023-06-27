02de3bdb66fa02e9816ad8f161bc75bfe5e83ba3
kiritokunti <duman200354@mail.ru>
2023-06-27 16:02:04 +0600

    feat(homeview): make request on scroll down

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <template>
     <!-- Introduction -->
     <section class="mb-8 py-20 text-white text-center relative">
-        <div
-            class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-            style="background-image: url(assets/img/header.png)"
-        ></div>
+        <div class="absolute inset-0 w-full h-full bg-contain introduction-bg"
+            style="background-image: url(assets/img/header.png)"></div>
         <div class="container mx-auto">
             <div class="text-white main-header-content">
                 <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
@@ -17,10 +15,7 @@
             </div>
         </div>
 
-        <img
-            class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
-            src="/assets/img/introduction-music.png"
-        />
+        <img class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full" src="/assets/img/introduction-music.png" />
     </section>
 
     <!-- Main Content -->
@@ -42,7 +37,7 @@
 
 <script>
 import { db } from '@/includes/firebase'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, limit, startAfter, getDoc, doc, orderBy } from 'firebase/firestore'
 import SongItem from '@/components/SongItem.vue'
 
 export default {
@@ -52,16 +47,53 @@ export default {
     data() {
         return {
             songs: [],
+            maxPerPage: 5,
+            pendingRequest: false,
         }
     },
     async created() {
-        const snapshots = await getDocs(collection(db, 'songs'));
-        snapshots.forEach((document) => {
-            this.songs.push({
-                docID: document.id,
-                ...document.data(),
+        this.getSongs();
+
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    beforeUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll() {
+            const { scrollTop, offsetHeight } = document.documentElement;
+            const { innerHeight } = window;
+            const bottomOfWindow = Math.round(scrollTop) + innerHeight === offsetHeight;
+
+            if (bottomOfWindow) {
+                this.getSongs();
+            }
+        },
+        async getSongs() {
+            if (this.pendingRequest) {
+                return;
+            }
+
+            this.pendingRequest = true;
+
+            let querySongs;
+            if (this.songs.length > 0) {
+                const lastDoc = await getDoc(doc(db, 'songs', this.songs[this.songs.length - 1].docID));
+                querySongs = query(collection(db, 'songs'), orderBy('modified_name'), startAfter(lastDoc), limit(this.maxPerPage))
+            } else {
+                querySongs = query(collection(db, 'songs'), orderBy('modified_name'), limit(this.maxPerPage))
+            }
+            const snapshots = await getDocs(querySongs);
+
+            snapshots.forEach((document) => {
+                this.songs.push({
+                    docID: document.id,
+                    ...document.data(),
+                })
             })
-        })
+
+            this.pendingRequest = false;
+        },
     }
 }
 </script>
