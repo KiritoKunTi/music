eea1dc3b2213e429641c4ad19d4819d724786c52
kiritokunti <duman200354@mail.ru>
2023-06-30 10:47:16 +0600

    feat(songview): sort comments

--- src/views/SongView.vue
+++ src/views/SongView.vue
@@ -35,7 +35,7 @@
                         </button>
                     </vee-form>
                     <!-- Sort Comments -->
-                    <select class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" >
+                    <select class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded" v-model="sort">
                         <option value="1">Latest</option>
                         <option value="2">Oldest</option>
                     </select>
@@ -44,7 +44,7 @@
         </section>
         <!-- Comments -->
         <ul class="container mx-auto">
-            <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in comments" :key="comment.docID">
+            <li class="p-6 bg-gray-50 border border-gray-200" v-for="comment in sortedComments" :key="comment.docID">
                 <!-- Comment Author -->
                 <div class="mb-5">
                     <div class="font-bold">{{ comment.name }}</div>
@@ -70,6 +70,7 @@ export default {
             schema: {
                 comment: 'required|min:3',
             },
+            sort: '1',
             comment_in_submission: false,
             comment_show_alert: false,
             comment_alert_varialt: 'bg-blue-500',
@@ -77,7 +78,15 @@ export default {
         }
     },
     computed: {
-        ...mapState(useUserStore, ['userLoggedIn'])
+        ...mapState(useUserStore, ['userLoggedIn']),
+        sortedComments() {
+            return this.comments.slice().sort((a, b) => {
+                if(this.sort === '1') {
+                    return new Date(b.datePosted) - new Date(a.datePosted);
+                }
+                return new Date(a.datePosted) - new Date(b.datePosted);
+            })
+        }
     },
     async created() {
         this.getSong();
