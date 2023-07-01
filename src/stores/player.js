de5f7fdaf3e42e6082425e7a3b19be4213f26d57
kiritokunti <duman200354@mail.ru>
2023-07-01 15:25:45 +0600

    feat(player): display time by formatting

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,10 +1,14 @@
 import { defineStore } from "pinia";
 import { Howl } from 'howler'
+import helper from '@/includes/helper'
+
 
 export default defineStore('player', {
     state: () => ({
         current_song: {},
         sound: {},
+        seek: '00:00',
+        duration: '00:00',
     }),
     getters: {
         playing: (state) => {
@@ -23,6 +27,10 @@ export default defineStore('player', {
             });
 
             this.sound.play();
+
+            this.sound.on('play', () => {
+                requestAnimationFrame(this.progress)
+            })
         }, 
         async toggleAudio() {
             if(!this.sound.playing) {
@@ -33,6 +41,14 @@ export default defineStore('player', {
                 this.sound.pause();
             } else {
                 this.sound.play();
+            }
+        },
+        progress() {
+            this.seek = helper.formatTime(this.sound.seek());
+            this.duration = helper.formatTime(this.sound.duration());
+
+            if(this.sound.playing()) {
+                requestAnimationFrame(this.progress)
             }
         }
     }
