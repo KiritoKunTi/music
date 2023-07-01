8544bcb32be856fdf4874ee5677cc5fd75125bb3
kiritokunti <duman200354@mail.ru>
2023-07-01 16:01:40 +0600

    feat(player): show song info, and progress bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -5,10 +5,11 @@ import helper from '@/includes/helper'
 
 export default defineStore('player', {
     state: () => ({
-        current_song: {},
+        currentSong: {},
         sound: {},
         seek: '00:00',
         duration: '00:00',
+        playerProgress: '0%',
     }),
     getters: {
         playing: (state) => {
@@ -20,7 +21,11 @@ export default defineStore('player', {
     },
     actions: {
         async newSong(song) {
-            this.current_song = song;
+            if(this.sound instanceof Howl) {
+                this.sound.unload();
+            }
+
+            this.currentSong = song;
             this.sound = new Howl({
                 src: [song.url],
                 html5: true,
@@ -46,6 +51,8 @@ export default defineStore('player', {
         progress() {
             this.seek = helper.formatTime(this.sound.seek());
             this.duration = helper.formatTime(this.sound.duration());
+
+            this.playerProgress = `${(this.sound.seek() / this.sound.duration() * 100)}%`
 
             if(this.sound.playing()) {
                 requestAnimationFrame(this.progress)
